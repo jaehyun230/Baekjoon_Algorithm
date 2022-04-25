dcf2b746d87b19b27978aec853ba192834c9eb64
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-04-25 11:03:16 +0900

    [Gold V] Title: 파이프 옮기기 2, Time: 68 ms, Memory: 30852 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.py
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.py
@@ -21,13 +21,11 @@ shape_r[0][1] = 1
 for i in range (n) :
   for j in range (2, n) :
     if graph[i][j] == 0 :
-      if j-1 >= 0 :
-        shape_r[i][j] = shape_r[i][j-1] + shape_c[i][j-1]
+      shape_r[i][j] = shape_r[i][j-1] + shape_c[i][j-1]
     if graph[i][j] == 0 :
-      if i-1 >= 0 :
-        shape_d[i][j] = shape_d[i-1][j] + shape_c[i-1][j]
+      shape_d[i][j] = shape_d[i-1][j] + shape_c[i-1][j]
     if graph[i][j] == 0 and graph[i-1][j] == 0 and graph[i][j-1] == 0 :
-      if i-1 >= 0 and j-1 >= 0 :
+      if i-1 >= 0 :
         shape_c[i][j] = shape_c[i-1][j-1] + shape_r[i-1][j-1] + shape_d[i-1][j-1]
 
 print(shape_r[n-1][n-1]+shape_c[n-1][n-1]+shape_d[n-1][n-1])
