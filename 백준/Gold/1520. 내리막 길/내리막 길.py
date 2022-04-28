584ddcf31499194ec57fc7f197f27ef5532895d7
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-04-28 15:14:55 +0900

    [Gold IV] Title: 내리막 길, Time: 164 ms, Memory: 42548 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,29 +1,38 @@
 import sys
-sys.setrecursionlimit(10 ** 8)
+sys.setrecursionlimit(10 **8)
 input = sys.stdin.readline
- 
-def dfs(sx, sy):
-  # 도착 지점에 도달하면 1(한 가지 경우의 수)를 리턴
-  if sx == m-1 and sy == n-1:
+
+
+
+def dfs(x, y) :
+
+  if x == n-1 and y == m-1 :
     return 1
- 
-  # 이미 방문한 적이 있다면 그 위치에서 출발하는 경우의 수를 리턴
-  if dp[sx][sy] != -1:
-    return dp[sx][sy]
-    
-  ways = 0
-  for i in range(4):
-    nx, ny = sx + dx[i], sy + dy[i]
-    if 0 <= nx < m and 0 <= ny < n and graph[sx][sy] > graph[nx][ny]:
-       ways += dfs(nx, ny)
-    
-  dp[sx][sy] = ways
-  return dp[sx][sy]
- 
- 
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(m)]
-dp = [[-1] * n for _ in range(m)]
-dx, dy = [1,-1,0,0], [0,0,1,-1]
- 
+
+  if visited[x][y] != -1 :
+    return visited[x][y]
+
+  way = 0
+  for i in range(4) :
+    mx = x+dx[i]
+    my = y+dy[i]
+    if 0 <= mx < n and 0 <= my < m :
+      if graph[x][y] > graph[mx][my] :
+        way += dfs(mx, my)
+
+  visited[x][y] = way
+  return visited[x][y]
+
+n, m = map(int, input().split())
+
+graph = []
+
+for _ in range(n) :
+  graph.append(list(map(int, input().split())))
+
+visited = [[-1]*m for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+     
 print(dfs(0,0))
