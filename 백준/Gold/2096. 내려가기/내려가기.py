fa6f8996c153169dfbb96bedeeeba4bdc79d6649
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-05-02 11:18:06 +0900

    [Gold IV] Title: 내려가기, Time: 4612 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+max_dp = [0] * 3
+min_dp = [0] * 3
+
+for _ in range(1) :
+  x, y, z = map(int, input().split())
+  max_dp[0], max_dp[1], max_dp[2] = x, y, z
+  min_dp[0], min_dp[1], min_dp[2] = x, y, z
+
+for i in range (1, n) :
+  temp = [0] * 3
+  x, y, z = map(int, input().split())
+  temp[0] = max(max_dp[0], max_dp[1]) + x
+  temp[1] = max(max_dp[0], max_dp[1], max_dp[2]) + y
+  temp[2] = max(max_dp[1], max_dp[2]) + z
+
+  for j in range (3) :
+    max_dp[j] = temp[j]
+  
+  temp[0] = min(min_dp[0], min_dp[1]) + x
+  temp[1] = min(min_dp[0], min_dp[1], min_dp[2]) + y
+  temp[2] = min(min_dp[1], min_dp[2]) + z
+
+  for j in range (3) :
+    min_dp[j] = temp[j]
+
+print(max(max_dp), min(min_dp))
