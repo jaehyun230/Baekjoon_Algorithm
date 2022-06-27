01e26b5b34f4fd33cbadebcdb424a79ec520b507
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-06-27 11:57:56 +0900

    [Silver IV] Title: 국영수, Time: 464 ms, Memory: 79264 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,9 @@
+import sys
+
+n = int(sys.stdin.readline())
+table = [list(sys.stdin.readline().split()) for _ in range(n)]
+
+table.sort(key = lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in table:
+  sys.stdout.write(str(student[0]) + "\n")
