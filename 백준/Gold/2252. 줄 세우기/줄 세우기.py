465746815bc7eca8d441c23d821fb2f3dfc933d2
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-06-28 11:31:25 +0900

    [Gold III] Title: 줄 세우기, Time: 244 ms, Memory: 37504 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -5,25 +5,26 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-
 indegree = [0] * (n+1)
-graph = [[] for _ in range(n+1)]
-q = deque()
+graph = [[] for _ in range (n+1)]
 
-for _ in range(m) :
-  x, y = map(int, input().split())
-  graph[x].append(y)
-  indegree[y] +=1
+for _ in range (m) :
+  start, end = map(int, input().split())
+  indegree[end] +=1
+  graph[start].append(end)
 
-for i in range(1, n+1) :
-  if indegree[i] == 0 :
-    q.append(i)
+def topology_sort() :
+  q = deque()
+  for k in range (1, n+1) :
+    if indegree[k] == 0 :
+      q.append(k)
 
-while q :
-  student = q.popleft()
+  while q :
+    now = q.popleft()
+    for i in graph[now] :
+      indegree[i] -=1
+      if indegree[i] == 0 :
+        q.append(i)
+    print(now, end = " ")
 
-  for i in graph[student] :
-    indegree[i] -=1
-    if indegree[i] == 0 :
-      q.append(i)
-  print(student, end=" ")
+topology_sort()
