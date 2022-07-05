5cab2bba293fa5dbafab9c616a3b04935b597879
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-07-05 12:01:10 +0900

    [Gold III] Title: 앱, Time: 716 ms, Memory: 47868 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,23 +1,30 @@
-N, M = map(int, input().split())
-A = [0] + list(map(int, input().split())) #byte
-C = [0] + list(map(int, input().split())) #cost
-dp = [[0 for _ in range(sum(C)+1)] for _ in range(N+1)] #냅색알고리즘이 실행될 dp
-result = sum(C) #열의 최댓값
+import sys
 
-for i in range(1, N+1):
-    byte = A[i]
-    cost = C[i]
-    
-    for j in range(1, sum(C) + 1):
-        if j < cost: #현재 앱을 비활성화할만큼의 cost가 충분하지 않을 경우
-            dp[i][j] = dp[i-1][j]
-        else:
-        	#같은 cost 내에서 현재 앱을 끈 뒤의 byte와 현재 앱을 끄지 않은 뒤의 byte를 비교
-            dp[i][j] = max(byte + dp[i-1][j-cost], dp[i-1][j])
-            
-        if dp[i][j] >= M: #조건이 충족된다면
-            result = min(result, j) #더 작은 cost값으로 갱신
-if M != 0:
-    print(result)
-else:
-    print(0)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+use = [0] + list(map(int, input().split()))
+
+c = [0] + list(map(int, input().split()))
+result = sum(c)
+
+dp = [[0 for _ in range(sum(c)+1)] for _ in range(n+1)]
+
+for i in range (1, n+1) :
+  byte = use[i]
+  cost = c[i]
+  
+  for j in range (1, sum(c)+1) :
+    if j < cost :
+      dp[i][j] = dp[i-1][j]
+    else :
+      dp[i][j] = max(byte + dp[i-1][j-cost], dp[i-1][j])
+
+    if dp[i][j] >= m :
+      result = min(result, j)
+
+if m !=0 :
+  print(result)
+else :
+  print(0)
