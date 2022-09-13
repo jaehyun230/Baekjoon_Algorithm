2a8f261c54e5468746612279df5a3c44f3a67c39
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-09-13 15:30:47 +0900

    [level 3] Title: 징검다리 건너기, Time: 99.07 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,26 @@
+def solution(stones, k):  
+    start = min(stones)
+    end = max(stones)
+
+    def check(num) :
+        count = k
+        for i in range(len(stones)) :
+            if stones[i] < num :
+                count -=1
+                if count == 0 :
+                    return False
+            else :
+                count = k
+        return True
+    
+    while start <= end :
+        mid = (start+end)//2
+        
+        if check(mid) == False :
+            end = mid - 1
+            
+        else :
+            start = mid + 1
+            
+            
+    return start-1
