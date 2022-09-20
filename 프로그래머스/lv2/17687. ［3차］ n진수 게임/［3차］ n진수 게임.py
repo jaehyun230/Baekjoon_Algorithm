5fc92df6ce744a825aa4fa6e66a3f0976ebbe590
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-09-20 17:07:48 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.35 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -0,0 +1,38 @@
+#진법, 구할숫자갯수, 참가인원, 순서
+def solution(n, t, m, p): 
+    answer = ''
+    
+    #현재 숫자
+    now = 0
+    #변환진수 남은 숫자갯수
+    counter = 1
+    #순서
+    order = p
+    
+    def change(num, n) :
+        numbers = '0123456789ABCDEF'
+        result = ''
+        if num == 0 :
+            return '0'
+        while num > 0 :
+            result += numbers[num%n]
+            num = num // n
+        
+        return result
+    
+    now_num = '0'
+    while t > 0 :
+        counter -=1
+        order -=1
+        
+        if order == 0 :
+            answer += now_num[counter]
+            order = m
+            t -=1
+        
+        if counter == 0 :
+            now +=1
+            now_num = change(now, n)
+            counter = len(now_num)
+            
+    return answer
