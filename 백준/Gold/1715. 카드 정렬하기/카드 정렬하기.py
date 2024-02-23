1417a282f706535758ac587dd501b26d9cf0b922
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2024-02-23 14:54:57 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 180 ms, Memory: 34360 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,21 +1,22 @@
 import heapq
-
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
 
-deck = []
+q = []
 
-for _ in range (n) :
-  heapq.heappush(deck, int(input()))
+for _ in range(n):
+    q.append(int(input()))
+
+q.sort()
 
 answer = 0
-while len(deck) >= 2 :
-  card1 = heapq.heappop(deck)
-  card2 = heapq.heappop(deck)
-  answer += card1 + card2
-  heapq.heappush(deck, card1+card2)
+while len(q) >= 2:
+    a = heapq.heappop(q)
+    b = heapq.heappop(q)
+    answer += a + b
+    heapq.heappush(q, a + b)
 
 print(answer)
