be04583522ece702867587062284fbb1eadf080f
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2024-04-06 11:15:47 +0900

    [Gold IV] Title: 뱀, Time: 76 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,74 +1,71 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
-#판 크기
+
 n = int(input())
-#사과 개수
+
 k = int(input())
-#방향 전환 정보 리스트
-direction_list = deque()
-#뱀의 위치
-snake = deque()
-#게임판
-graph = [[0]*(n+1) for _ in range(n+1)]
+
+visited = [[False] * n for _ in range(n)]
+graph = [[0]*n for _ in range(n)]
 
 dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+dy = [0, 1, 0, -1]
 
 for _ in range(k) :
-  y, x = map(int, input().split())
-  #사과 위치정보 추가
-  graph[y][x] = 2
+    #사과 위치
+    a, b = map(int, input().split())
+    graph[a-1][b-1] = 1
 
-#방향 전환 정보 개수
 l = int(input())
 
-for _ in range(l) :
-  time, direction = input().split()
-  #방향 전환 정보 추가
-  direction_list.append((int(time), direction))
+q = deque()
+q.append((0, 0, 1))
+visited[0][0] = True
+now = 0
+alive = True
 
-play_time = 0
-change_time = int(1e9)
-now = 2
+def move(x1,y1, direct) :
+    mx = x1 + dx[direct]
+    my = y1 + dy[direct]
+    q.append((x1, y1, direct))
 
-#뱀 머리 위치
-head = [1, 1]
-snake.append((1, 1))
-while True :
-  if direction_list :
-    change_time = direction_list[0][0]
-  else :
-    change_time = int(1e9)
-  if change_time == play_time :
-    if direction_list[0][1] == "L" :
-      now -=1
-      if now < 0 :
-        now = 3
+    if 0 <= mx < n and 0<= my < n and visited[mx][my] == False :
+        visited[mx][my] = True
+        if graph[mx][my] == 1 :
+            graph[mx][my] = 0
+        else :
+            #꼬리 제거
+            x2, y2, d2 = q.popleft()
+            visited[x2][y2] = False
+        q.append((mx, my, direct))
     else :
-      now +=1
-      if now > 3 :
-        now = 0
-    direction_list.popleft()
 
-  play_time +=1
-  
-  head[0], head[1] = head[0]+dy[now], head[1]+dx[now]
-  
-  snake.append((head[0], head[1]))
-  #벽을 부딛힐 경우
-  if head[0] > n or head[0] < 1 or head[1] > n or head[1] < 1 :
-    break
-  #자기 몸통에 부딛힐 경우
-  elif graph[head[0]][head[1]] == 1 :
-    break
-  #빈공간으로 이동할 경우
-  elif graph[head[0]][head[1]] == 0 :
-    graph[head[0]][head[1]] = 1
-    tail = snake.popleft()
-    graph[tail[0]][tail[1]] = 0
-  #사과를 먹은 경우 
-  elif graph[head[0]][head[1]] == 2 :
-    graph[head[0]][head[1]] = 1
-  
-print(play_time)
+        return False
+    return True
+
+for _ in range(l) :
+    t, d = input().split()
+    t = int(t)
+
+    while now < t and alive == True :
+        x, y, direction = q.pop()
+
+        if move(x, y, direction) == False :
+            alive = False
+        now +=1
+
+    if now == t :
+        x, y, direction = q.pop()
+        if d == 'L':
+            direction = (direction - 1) % 4
+        elif d == "D":
+            direction = (direction + 1) % 4
+        q.append((x, y, direction))
+
+while alive == True :
+    x, y, direction = q.pop()
+    if move(x, y, direction) == False:
+        alive = False
+    now += 1
+
+print(now)
