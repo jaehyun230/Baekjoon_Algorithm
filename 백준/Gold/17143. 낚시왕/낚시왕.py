40f913207becea910381275a88d9ecfb609174dd
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2024-04-06 22:09:46 +0900

    [Gold I] Title: 낚시왕, Time: 2888 ms, Memory: 219912 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -1,71 +1,101 @@
-import sys
+n, m, k = map(int, input().split())
 
-input = sys.stdin.readline
+shark = []
+shark_alive = [True] * k
 
-# 상어 이동 함수
-def move_shark():
-    g = [[[] for _ in range(C)] for _ in range(R)]
-    for i in range(R):
-        for j in range(C):
-            if graph[i][j]:
-                x, y = i, j
-                s, d, z = graph[i][j][0]
-                dist = s
-                # 상어의 속도만큼 1칸씩 이동
-                while 0 < dist:
-                    nx = x + direction[d][0]
-                    ny = y + direction[d][1]
-                    # 맵 내부에서 이동하는 경우
-                    if 0 <= nx < R and 0 <= ny < C:
-                        x, y = nx, ny
-                        dist -= 1 # 이동해야 할 남은 거리 1 차감
-                    # 벽과 충돌하는 경우, 현재 진행방향별 방향전환
-                    else:
-                        # d(0-1-2-3) : 방향(상-하-우-좌)
-                        # 상 to 하 or 우 to 좌
-                        if d == 0 or d == 2:
-                            d += 1
-                        # 하 to 상 or 좌 to 우
-                        elif d == 1 or d == 3:
-                            d -= 1
-                        continue
-                g[x][y].append([s, d, z])
+graph = [
+    [[] for _ in range(m)] for _ in range(n)]
 
-    for i in range(R):
-        for j in range(C):
-            graph[i][j] = g[i][j]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 
-# 상어 낚시 함수
-def catch_shark():
-    global answer
-    # 낚시왕이 칼럼 방향으로 이동하며 낚시를 하기 때문에 칼럼 우선순위
-    for i in range(C):
-        for j in range(R):
-            # 상어가 존재하는 경우
-            if graph[j][i]:
-                answer += graph[j][i][0][2]
-                graph[j][i].remove(graph[j][i][0])
+for idx in range(k) :
+    # 상어 위치, 속력, 방향, 크기
+    r, c, s, d, z = map(int, input().split())
+    shark.append([r-1, c-1, s, d-1, z, idx])
+    # 해당위치 idx 상어 존재
+    graph[r-1][c-1].append(idx)
+
+def change_d(d) :
+    if d == 0:
+        return 1
+    elif d == 1:
+        return 0
+    elif d == 2:
+        return 3
+    elif d == 3:
+        return 2
+
+def fight(n1, n2) :
+
+    r, c, s, d, z, idx = shark[n1]
+    r2, c2, s2, d2, z2, idx2 = shark[n2]
+
+    if z > z2 :
+        shark_alive[idx2] = False
+        return idx
+    else :
+        shark_alive[idx] = False
+        return idx2
+
+# 상어 상태 업데이트
+def update(r, c, s, d, z, idx) :
+    shark[idx] = [r, c, s, d, z, idx]
+
+def shark_move() :
+    global graph
+
+    new_graph = [
+    [[] for _ in range(m)] for _ in range(n)]
+
+    for i in range(k) :
+        if shark_alive[i] == True :
+            r, c, s, d, z, idx = shark[i]
+
+            for _ in range(s) :
+                mr = r + dx[d]
+                mc = c + dy[d]
+                if 0 <= mr < n and 0 <= mc < m :
+                    r, c = mr, mc
+                else :
+                    d = change_d(d)
+                    r = r +dx[d]
+                    c = c + dy[d]
+
+            update(r, c, s, d, z, idx)
+
+            # 지금 잘 못된 부분이 모든 이동후 이루어져야함 싸우는 건
+            # 이동 후 해당 위치에 상어가 있으면 큰 애가 살아남게 하기
+            if new_graph[r][c] :
+                idx2 = new_graph[r][c].pop()
+                # 해당 상어가 자신일 경우
+                if idx == idx2 :
+                    new_graph[r][c].append(idx)
+                else :
+                    num = fight(idx, idx2)
+                    new_graph[r][c].append(num)
+            # 해당 위치 상어 없으면 상어 생존
+            else :
+                new_graph[r][c].append(idx)
+
+    graph = new_graph
+
+
+start = 0
+answer = 0
+
+while start < m :
+    # print(graph)
+    for i in range(n) :
+        if graph[i][start] :
+            shark_num = graph[i][start].pop()
+            if shark_alive[shark_num] == True :
+                # print(shark_num, shark[shark_num][4], i, start)
+                answer += shark[shark_num][4]
+                shark_alive[shark_num] = False
                 break
 
-        move_shark()
-        for m in range(R):
-            for n in range(C):
-                # 한 좌표에 상어가 2마리 이상 있는 경우
-                if 1 < len(graph[m][n]):
-                    # 몸집이 작은 순서대로 상어 제거
-                    graph[m][n].sort(key=lambda x: x[2], reverse=True)
-                    while 1 < len(graph[m][n]):
-                        graph[m][n].pop()
+    shark_move()
+    start +=1
 
-if __name__ == '__main__':
-    R, C, M = map(int, input().split())
-    # 진행방향: 상, 하, 우, 좌
-    direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    graph = [[[] for _ in range(C)] for _ in range(R)]
-    for _ in range(M):
-        r, c, s, d, z = map(int, input().split())
-        graph[r-1][c-1].append([s, d-1, z])
-
-    answer = 0
-    catch_shark()
-    print(answer)
+print(answer)
